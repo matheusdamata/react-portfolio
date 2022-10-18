4744bd4912ab7212ddcd1d8960339da6d42381c0
Matheus da Mata <matheusdamatag@gmail.com>
2022-10-18 14:34:16 -0300

    remove lazy loading

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,29 +1,34 @@
-import HeaderMenu from './header-menu';
-import { LazyLoadComponent  } from 'react-lazy-load-image-component';
+import HeaderMenu from "./header-menu";
 
 import * as C from "./styles";
 
-import ProfileImage from '../../assets/profile.svg';
+import ProfileImage from "../../assets/profile.svg";
 
 const About = () => {
   const loadingLazy = () => {
-    console.log('Carregou')
-  }
+    console.log("Carregou");
+  };
 
   return (
     <C.Container id="app">
       <HeaderMenu />
 
-      <C.InfoArea className="progressive">
-        <LazyLoadComponent delayMethod="debounce" beforeLoad={loadingLazy} delayTime={5000}>
-          <C.InfoImageArea>
-            <img className="preview lazy" src={ProfileImage} alt="Profile image" width={350} height={350} />
-          </C.InfoImageArea>
-        </LazyLoadComponent>
-        
+      <C.InfoArea>
+        <C.InfoImageArea>
+          <img
+            className="preview lazy"
+            src={ProfileImage}
+            alt="Profile image"
+            width={350}
+            height={350}
+          />
+        </C.InfoImageArea>
+
         <C.InfoTexts>
-            <C.InfoGreeting>Olá! Eu sou</C.InfoGreeting>
-          <C.InfoHeroDescription><span>MATHEUS</span> DA MATA</C.InfoHeroDescription>
+          <C.InfoGreeting>Olá! Eu sou</C.InfoGreeting>
+          <C.InfoHeroDescription>
+            <span>MATHEUS</span> DA MATA
+          </C.InfoHeroDescription>
           <C.InfoSmallBio>
             Desenvolvedor <span>Front-end</span> apaixonado por criar
             aplicativos e experiências interativas na web.
