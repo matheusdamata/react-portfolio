4f0f09addb0884752635f0be0fb55aa6d6a35b0c
Matheus da Mata <matheusdamatag@gmail.com>
2022-12-15 16:35:48 -0300

    updated: text about me.

--- src/components/aboutme/index.tsx
+++ src/components/aboutme/index.tsx
@@ -8,9 +8,10 @@ const AboutMe = () => {
                 <C.AboutMeText>
                     Meu nome é Matheus, moro em Goiânia - Goiás.
                     <br /><br />
-                    {/*Empresário no ramo de bebê, no momento em transição de carreira para Desenvolvedor Front-end, 
-                    deixando minha esposa responsável pela nossa empresa.
-                    <br /><br /> */}
+                    Sou fundador da @usesmilebaby, amante da tecnologia e inovação!
+                    <br /><br />
+                    Por gostar muito de criar algo, aos 15 anos inicei minha empresa pegando um produto já existente e melhorando ele em alguns aspectos, com o objetivo em oferecer o menor custo com a melhor qualidade possível para o B2B.
+                    <br /><br />
                     Aos 10 anos de idade fiz meu primeiro "Hello World" em HTML, 
                     e me apaixonei por programação, sou um desenvolvedor entusiasta, autodidata, que adora inovar, aprender e criar algo novo. 
                     Atualmente realizo projetos (pessoais) utilizando ReactJS, NextJS, React Native, HTML, CSS, JavaScript e TypeScript.
